029d5e949957d57b2544f340c1af1c0f3c61e21d
girishdigge <girishdigge@gmail.com>
2024-11-13 21:06:44 +0530

    hero section with search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,25 @@
-const page = () => {
-  return <div>Home</div>;
+import SearchForm from '../../components/SearchForm';
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading '>
+          Pitch your Startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
 };
-export default page;
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss';
+import tailwindAnimate from 'tailwindcss-animate';
+import typography from '@tailwindcss/typography';
 
 const config: Config = {
   darkMode: ['class'],
@@ -29,6 +31,38 @@ const config: Config = {
           '100': '#F7F7F7',
           DEFAULT: '#FFFFFF',
         },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       fontFamily: {
         'work-sans': ['var(--font-work-sans)'],
@@ -45,7 +79,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [tailwindAnimate, typography],
 };
 
 export default config;
